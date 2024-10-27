30598420563dd5c22562f7b05510e6f45a921b9d
rahul-sequelstring <rahul.kushwah@sequelstring.com>
2024-10-27 07:14:27 +0530

    deployment of react js

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,50 @@
 import "./db/db";
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import errorMiddleware from "./modules/config/errorHandler";
 import importRoutings from "./routing/routingIndex";
 import http from "http";
-import * as path from 'path'
+import * as path from "path";
 import { setupSocket } from "./modules/chatSocket/chatSocket";
 import { statusCode } from "./config/helper/statusCode";
-// import './learning'
 
-const app = express();
 dotenv.config();
 
-// create the server
+const app = express();
+
+// Create the server
 const server = http.createServer(app);
 setupSocket(server);
 
-// use the body-parser
+// Use body-parser
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/notes', express.static(path.join(__dirname,'src','../public/notes')));
-
+app.use('/notes', express.static(path.join(__dirname, 'src', '../public/notes')));
 
 // Enable CORS for all routes and all origins
-app.use(cors({
-  origin: "*",
+const corsOptions = {
+  origin: process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_BASE_PROD_URL
+      : "*",
   methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
-}));
+  credentials: true, // Allow cookies to be sent
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions));
 
-// import routing function
+
+// Import routing function
 importRoutings(app);
 
-app.use('/',(req,res) => {
-  res.status(statusCode.info).send("Welcome to our app")
-})
+app.use('/', (req: Request, res: Response) => {
+  res.status(statusCode.info).send("Welcome to our app");
+});
 
 
-// To handle the error which is throw by the next
 app.use(errorMiddleware);
-
 
 server.listen(process.env.PORT, () => {
   console.log(`The server is running on port ${process.env.PORT}`);
 });
-
-const today = new Date().getDay();
-if (today === 0 || today === 6) {
-  console.log('Today is a weekend. Punching in/out is not allowed.');
-} else {
-  console.log('Today is a weekday. Punching in/out is allowed.');
-}
